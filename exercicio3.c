508476fcd9c67f1b6893922ab10050c50b4fb6c0
BrunoMaximo03 <brunomaximobt@gmail.com>
2025-02-06 15:35:31 -0300

    Pegando a primeira posicao do array por ponteiro e lendo todos os valores pelo memsmo ponteiro

---
+++ exercicio3.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+    int main() {
+
+     int array[10] = {1,2,3,4,5,6,7,8,9,10};
+     int* ponteiro = &array;
+
+     for(int i = 0; i < 10; i++) {
+        printf("Valores -> %d",ponteiro[i]);
+     }
+     return 0;
+    }
