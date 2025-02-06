b4201fd7ab60dfef65468a27294547612256fa17
BrunoMaximo03 <brunomaximobt@gmail.com>
2025-02-06 17:28:50 -0300

    Revisando conceitos básicos

--- exercicio1.c
+++ exercicio1.c
@@ -8,6 +8,7 @@
         int* ponteiro = &var;  // ponteiro que aponta para o endereço de var;
 
         printf("O valor eh %d",*ponteiro); // foi usado * para acessar o que tem no endereço, no caso o que tem no endereço de var que é 10
+        free(ponteiro);
 
         return 0;
     }
