e016ea033f70752908ec43ba60cf330606385d03
BrunoMaximo03 <brunomaximobt@gmail.com>
2025-02-06 17:23:25 -0300

    Usando o ponteiro para percorrer a string e contar quantos caracteres ela possui.

---
+++ exercicio6.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main() {
+
+    char str[] = "Ola mundo";
+    char* ponteiro = str; //aponta para o primeiro endereço de memória do array que no caso é o O
+    int conta = 0;
+    str[strcspn(str,"\n")] = '\0';
+
+
+    for(int i = 0; i < strlen(str); i++) {
+        if(*(ponteiro + i) != '\0') {
+            conta++;
+        }
+    }
+    printf("Existem %d caracteres na string\n",conta);
+
+
+
+    return 0;
+}
